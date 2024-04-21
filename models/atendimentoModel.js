340dc14898891584e8fcab2dedc800d86e53d5a4
Alanz0ka <jaas4@aluno.ifal.edu.br>
2024-04-21 19:35:21 -0300

    Adicionando herança

--- models/atendimentoModel.js
+++ models/atendimentoModel.js
@@ -1,59 +1,35 @@
+const { response } = require("express")
 const conexao = require("../infraestrutura/conexão")
 class AtendimentoModel {
-    listar(){
-        const sql = "SELECT * FROM atendimentos"
-        return new Promise((resolve, reject)=>{
-            conexao.query(sql, {}, (error, resposta)=> {
-                if (error){
-                    console.log("Erro no listar:", error);
-                    reject(error);
+    
+    executeQuery(sql, parametros = ""){
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, parametros, (error, resposta) =>{
+                if(error){
+                    return reject(error);
                 }
-                console.log("Resultado da consulta no listar:", resposta);
-                resolve(resposta);
+                return resolve(resposta);
             })
         })
+    }
+    listar(){
+        const sql = "SELECT * FROM atendimentos"
+        return this.executeQuery(sql);
     }
 
     criar(novoAtendimento){
         const sql = "INSERT INTO atendimentos SET ?";
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, novoAtendimento, (error, resposta) => {
-                if(error){
-                    console.log("Erro ao criar", error)
-                    reject(error);
-                }
-                console.log("Criado com sucesso")
-                resolve(resposta)
-            })
-        })
+        return this.executeQuery(sql, novoAtendimento)
     }
 
     atualizar(atendimentoAtualizado, id){
         const sql = "update atendimentos set ? where id = ?";
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, [atendimentoAtualizado, id ], (error, resposta) => {
-                if(error){
-                    console.log("Erro ao criar", error)
-                    reject(error);
-                }
-                console.log("Criado com sucesso")
-                resolve(resposta)
-            })
-        })
+        return this.executeQuery(sql, [atendimentoAtualizado, id])
     }
 
     delete(id){
         const sql = "delete from atendimentos where id = ?";
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id , (error, resposta) => {
-                if(error){
-                    console.log("Erro ao apagar", error)
-                    reject(error);
-                }
-                console.log("Apagado com sucesso")
-                resolve(resposta)
-            })
-        })
+        return this.executeQuery(sql, id)
     }
 }
 
