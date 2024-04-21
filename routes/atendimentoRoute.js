3c3f02c7aa77e80627e466df4133780e6454d601
Alanz0ka <jaas4@aluno.ifal.edu.br>
2024-04-21 20:15:31 -0300

    Refatorando o código

--- routes/atendimentoRoute.js
+++ routes/atendimentoRoute.js
@@ -4,31 +4,12 @@ const router = Router();
 const AtendimentoController = require("../controllers/AtendimentoController");
 const atendimentoController = require("../controllers/atendimentoController");
 
-router.get("/atendimentos", (req, res)=>{
-    const listaAtendimentos = AtendimentoController.buscar();
-    listaAtendimentos
-    .then((atendimentos) => res.status(200).json(atendimentos))
-    .catch((error) => res.status(400).json(error.message))
-});
+router.get("/atendimentos", atendimentoController.buscar);
 
-router.post("/atendimentos", (req,res)=>{
-    const  novoAtendimento = req.body;
-    const atendimento = atendimentoController.criar(novoAtendimento);
-    atendimento.then(atendimentoCriado => res.status(201).json(atendimentoCriado))
-    .catch(error => res.status(400).json(error.message));
-})
+router.post("/atendimentos", atendimentoController.criar)
 
-router.put("/atendimentos/:id", (req,res)=>{
-    const {id} = req.params;
-    const atendimentoAtualizado = req.body;
-    const atendimento = atendimentoController.atualizar(atendimentoAtualizado, id);
-    atendimento.then((resultAtendimentoAtualizado) => res.status(200).json(resultAtendimentoAtualizado)).catch((error) => res.status(400).json(error.message));
-})
+router.put("/atendimentos/:id", atendimentoController.atualizar)
 
-router.delete("/atendimentos/:id", (req,res)=>{
-    const {id} = req.params;
-    const atendimento = atendimentoController.delete(id);
-    atendimento.then((resultAtendimentoDeletado) => res.status(200).json(resultAtendimentoDeletado)).catch((error) => res.status(400).json(error.message));
-})
+router.delete("/atendimentos/:id", atendimentoController.delete)
 
 module.exports = router;
